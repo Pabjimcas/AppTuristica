a5635600d131a79c4bb03d521d7e406146781a70
Pablo <pabjimcas@gmail.com>
2016-06-01 14:51:22 +0200

    Location configurations

--- app/src/main/java/com/example/pabji/appturistica/activities/MainActivity.java
+++ app/src/main/java/com/example/pabji/appturistica/activities/MainActivity.java
@@ -1,18 +1,78 @@
 package com.example.pabji.appturistica.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.pabji.appturistica.R;
 import com.example.pabji.appturistica.fragments.MainFragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
+
+    private static final String TAG = MainActivity.class.getName();
+
+    private GoogleApiClient mGoogleApiClient;
+    public Location location;
+    private static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MainFragment mainFragment = MainFragment.newInstance();
-        getSupportFragmentManager().beginTransaction().replace(R.id.content,mainFragment).commit();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient = new GoogleApiClient.Builder(this, this, this).addApi(LocationServices.API).build();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED
+                    && checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+
+                Log.v(TAG, "Permission is granted");
+                location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            } else {
+                Log.v(TAG, "Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                        REQUEST_CODE);
+            }
+        } else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG, "Permission is granted");
+            location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        }
+        if(location != null) {
+            MainFragment mainFragment = MainFragment.newInstance(location);
+            getSupportFragmentManager().beginTransaction().replace(R.id.content, mainFragment).commit();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+
 }
--- app/src/main/java/com/example/pabji/appturistica/fragments/MainFragment.java
+++ app/src/main/java/com/example/pabji/appturistica/fragments/MainFragment.java
@@ -1,30 +1,54 @@
 package com.example.pabji.appturistica.fragments;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.pabji.appturistica.APIHelpers.TurismAPI;
+import com.example.pabji.appturistica.APIHelpers.TurismAPIClient;
 import com.example.pabji.appturistica.R;
+import com.example.pabji.appturistica.activities.MainActivity;
+import com.example.pabji.appturistica.models.RequestModel;
+import com.example.pabji.appturistica.models.ResponseModel;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by pabji on 01/06/2016.
  */
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment{
 
+    private static final String TAG = MainFragment.class.getName();
     @Bind(R.id.recycler_view)
     RecyclerView recyclerView;
 
-    public static MainFragment newInstance() {
+    private TurismAPI service;
+    private Location location;
+
+
+    public static MainFragment newInstance(Location location) {
         MainFragment fragment = new MainFragment();
         Bundle args = new Bundle();
+        args.putParcelable("location",location);
         fragment.setArguments(args);
         return fragment;
     }
@@ -37,5 +61,33 @@ public class MainFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         ButterKnife.bind(this, view);
+        location = getArguments().getParcelable("location");
+        getDataFromServer();
     }
+
+    private void getDataFromServer() {
+        service = TurismAPIClient.createService();
+        RequestModel request = new RequestModel("cc80e7fa7d839187f65d0f0c1d23d78e",String.valueOf(location.getLatitude()),String.valueOf(location.getLongitude()));
+        Call<ResponseModel> response = service.receiveSomePlaces("application/x-www-form-urlencoded",request.getKey(),request.getLatitude(),request.getLongitude());
+        response.enqueue(new Callback<ResponseModel>() {
+            @Override
+            public void onResponse(Call<ResponseModel> call, Response<ResponseModel> response) {
+                ResponseModel responseServer = response.body();
+                if (responseServer.getData().getTrips() !=null  && responseServer.getData().getDestinations()!=null){
+
+                    Log.d(TAG,"Tengo datos");
+
+                }else{
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseModel> call, Throwable t) {
+
+            }
+        });
+    }
+
+
 }
